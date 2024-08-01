30a73ec30674051583a0616491a56c80ace698ee
cgd8d <Claytongdavis@gmail.com>
2024-07-31 22:14:20 -0400

    Update PrimeGenerator.cpp

--- src/PrimeGenerator.cpp
+++ src/PrimeGenerator.cpp
@@ -489,12 +489,12 @@ void PrimeGenerator::fillNextPrimes_default(Vector<uint64_t>& primes, std::size_
         __m256i nextPrimes = _mm256_add_epi64(bitVals, low_vec);
         _mm256_storeu_si256((__m256i*)(primes.data()+j), nextPrimes);
 
-        /*std::cout<< "with low = "<<low<<":"<<std::endl;
+        std::cout<< "with low = "<<low<<":"<<std::endl;
         std::cout<< "bitindex0 = " << bitIndex0 << " --> p = " << primes[j+0]<<std::endl;
         std::cout<< "bitindex1 = " << bitIndex1 << " --> p = " << primes[j+1]<<std::endl;
         std::cout<< "bitindex2 = " << bitIndex2 << " --> p = " << primes[j+2]<<std::endl;
         std::cout<< "bitindex3 = " << bitIndex3 << " --> p = " << primes[j+3]<<std::endl;
-        std::cout<<std::endl;*/
+        std::cout<<std::endl;
           
         //primes[j+0] = nextPrime(bits, low); bits &= bits - 1;
         //primes[j+1] = nextPrime(bits, low); bits &= bits - 1;
@@ -503,7 +503,7 @@ void PrimeGenerator::fillNextPrimes_default(Vector<uint64_t>& primes, std::size_
         j += 4;
       }
       while (j < i);
-
+std::exit(0);
       low += 8 * 30;
       sieveIdx += 8;
     }
